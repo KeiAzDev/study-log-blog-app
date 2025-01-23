fde85a580c9a7a67185c476daec65aafbd4976d9
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-23 11:58:51 -0800

    /が何もないので/postsにリダイレクトするように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-
+import { redirect } from "next/navigation"
 
 export default function Home() {
+  redirect('/posts')
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       
