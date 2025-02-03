fdcdd29d96729293bd5dc01e66653d48e0bef5e8
KeiAzDev <enopiyo3code+work@gmail.com>
2025-02-02 21:58:24 -0800

    analytics実験

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { NextAuthProvider } from '@/components/providers/next-auth-provider'
 import Header from '@/components/layout/header'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,20 +24,24 @@ export default function RootLayout({
       <head>
         {gaId && (
           <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${gaId}');
-                `
-              }}
-            />
+            <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaId}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
           </>
         )}
       </head>
