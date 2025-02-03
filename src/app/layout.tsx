ef8149a007e23edc29deb75b25ea5845280a74a8
KeiAzDev <enopiyo3code+work@gmail.com>
2025-02-02 22:21:29 -0800

    アナリティクステスト

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,28 +24,28 @@ export default function RootLayout({
       <head>
         {gaId && (
           <>
+            {/* gtag.js の読み込み */}
             <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-        />
-        <Script
-          id="ga-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gaId}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+            />
+            {/* Google Analytics 設定スクリプト */}
+            <Script
+              id="google-analytics"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${gaId}');
+                `,
+              }}
+            />
           </>
         )}
       </head>
-      <body cz-shortcut-listen="true" className={inter.className}>
+      <body className={inter.className}>
         <NextAuthProvider>
           <Header />
           <main className="container mx-auto px-4 py-8">
